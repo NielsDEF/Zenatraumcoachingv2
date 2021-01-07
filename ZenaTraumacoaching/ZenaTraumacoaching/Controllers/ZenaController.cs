f8c27d52ce4d25fdc85c331b34822c5fda062b81
Niels Hoeke <niels_hoeke@hotmail.com>
2021-01-07 14:56:36 +0100

    blog en referentie v3

---
+++ ZenaTraumacoaching/ZenaTraumacoaching/Controllers/ZenaController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZenaTraumacoaching.Models;
+
+namespace ZenaTraumacoaching.Controllers
+{
+    public class ZenaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ReferentieToevoegenClick(ReferentieViewModel model)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult BlogToevoegenClick(BlogViewModel model)
+        {
+            return View();
+        }
+    }
+}
